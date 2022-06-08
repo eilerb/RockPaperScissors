2ca8995eea1e80c5ae96134115b0f2c8871cf1e4
eilerb <eiler.byberi@gmail.com>
2022-06-08 00:05:44 -0400

    Remove the game() function

--- game.js
+++ game.js
@@ -14,68 +14,52 @@ function computerPlay() {
     }
 }
 
-const str1 = 'Go again';
-const str2 = 'You Lose! Paper beats Rock';
-const str3 = 'You Lose! Rock beats Scissors';
-const str4 = 'You Lose! Scissors beat Paper';
-const str5 = 'You Win! Paper beats Rock';
-const str6 = 'You Win! Rock beats Scissors';
-const str7 = 'You Win! Scissors beat Paper';
+// const str1 = 'Tie';
+// const str2 = 'You Lose! Paper beats Rock';
+// const str3 = 'You Lose! Rock beats Scissors';
+// const str4 = 'You Lose! Scissors beat Paper';
+// const str5 = 'You Win! Paper beats Rock';
+// const str6 = 'You Win! Rock beats Scissors';
+// const str7 = 'You Win! Scissors beat Paper';
+
+const score = document.querySelector('score');
+let playerScore = 0;
+let compScore = 0;
+const victory = `You Won the Game! The Computer Lost! The score was ${playerScore} - ${compScore}`;
+const defeat = `You Lost the Game! The Computer Lost! The score was ${compScore} - ${playerScore}`;
 
 const btn = document.querySelectorAll('button');
+let result = btn.forEach(btn => {
+    playerSelection = btn.getAttribute('class');
+    btn.addEventListener('click', playRound)
+});
 
-function playRound(playerSelection, computerSelection) {
+score.textContent = `${playerScore} - ${compScore}`;
+
+function playRound() {
     computerSelection = computerPlay();
 
     if (playerSelection == computerSelection) {
-        return str1;
+        
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return str2;
+        compScore++;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return str6;
+        playerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return str5;
+        playerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return str4;
+        compScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return str7;
+        playerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return str3;
+        compScore++;
+    }
+
+    if (playerScore === 5) {
+        score.textContent = victory;
+        winner = true;
+    } else if (compScore === 5) {
+        score.textContent = defeat;
+        winner = true;
     }
 }
-
-function game() {
-    let playerScore = 0;
-    let compScore = 0;
-    const victory = `You Won the Game! The Computer Lost! The score was ${playerScore} - ${compScore}`;
-    const defeat = `You Lost the Game! The Computer Lost! The score was ${compScore} - ${playerScore}`;
-    let winner = false;
-
-    while (!winner) {
-        let result = btn.forEach(btn => {
-            playerSelection = btn.getAttribute('class');
-            btn.addEventListener('click', playRound)
-        });
-
-        const score = document.querySelector('score');
-
-        if (result === str1) {
-            score.textContent = `Tie! The score is ${playerScore} - ${compScore}`;
-        } else if (result === str2 || result === str3 || result === str4) {
-            compScore++;
-            score.textContent = `You Lost! The score is ${playerScore} - ${compScore}`;
-        } else if (result === str5 || result === str6 || result === str7) {
-            playerScore++;
-            score.textContent = `You won! The score is ${playerScore} - ${compScore}`;
-        }
-
-        if (playerScore === 5) {
-            score.textContent = victory;
-            winner = true;
-        } else if (compScore === 5) {
-            score.textContent = defeat;
-            winner = true;
-        }
-    }
-}
-
