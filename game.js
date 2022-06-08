2293a8e86cb49a5534b5dc0d748381e658d129de
eilerb <eiler.byberi@gmail.com>
2022-06-08 00:44:40 -0400

    Format the js file

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
+const btn = document.querySelectorAll('button');
+let playerScore = 0;
+let compScore = 0;
+score.textContent = '0 - 0';
+
 function computerPlay() {
     let a = 'rock';
     let b = 'paper';
@@ -14,21 +21,6 @@ function computerPlay() {
     }
 }
 
-// const str1 = 'Tie';
-// const str2 = 'You Lose! Paper beats Rock';
-// const str3 = 'You Lose! Rock beats Scissors';
-// const str4 = 'You Lose! Scissors beat Paper';
-// const str5 = 'You Win! Paper beats Rock';
-// const str6 = 'You Win! Rock beats Scissors';
-// const str7 = 'You Win! Scissors beat Paper';
-
-const score = document.querySelector('.score');
-const result = document.querySelector('.result');
-let playerScore = 0;
-let compScore = 0;
-
-const btn = document.querySelectorAll('button');
-
 if (playerScore < 5 || compScore < 5) {
     let round = btn.forEach(btn => {
         playerSelection = btn.getAttribute('class');
@@ -36,29 +28,27 @@ if (playerScore < 5 || compScore < 5) {
     });
 }
 
-score.textContent = '0 - 0';
-
 function playRound() {
     computerSelection = computerPlay();
 
-    if (playerSelection == computerSelection) {
-        
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    if (playerSelection === computerSelection) {
+
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         compScore++;
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         compScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         compScore++;
     }
 
     score.textContent = `${playerScore} - ${compScore}`;
-    
+
     if (playerScore === 5) {
         result.textContent = `You Won the Game!`;
         winner = true;
